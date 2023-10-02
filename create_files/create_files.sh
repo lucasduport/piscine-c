594ff52192d550ef655ab31c588db9ac23a0d0ef
Lucas Duport <lucas.duport@epita.fr>
2023-10-02 17:45:31 +0200

    create_files: tag number 1

---
+++ create_files/create_files.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+touch " "
+touch "\\"
+touch -- --
+touch \|
+touch '"'
+touch -- "--\$i*'\"\\"
+touch "# Exams are fun!"
+touch ";\`kill -9 0\`"
+
+for i in {1..50}
+do
+    mkdir $i
+    cd $i
+done
+touch farfaraway
