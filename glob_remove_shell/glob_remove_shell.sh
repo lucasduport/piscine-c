07b993278c2cc872dc8ffb406cee6d0d5e91b01b
Lucas Duport <lucas.duport@epita.fr>
2023-10-02 21:18:01 +0200

    glob_remove_shell: tag number 2

--- glob_remove_shell/glob_remove_shell.sh
+++ glob_remove_shell/glob_remove_shell.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 if [ $# -ne 1 ]; then
-  ext=txt
+    ext=txt
 else
-  ext=$1
+    ext=$1
 fi
 
-if [ $(echo *.${ext}) = "*.${ext}" ]; then
-  exit 1;
-else
-  for file in *.${ext}; do
-    rm -f $file
-  done
-fi
+for file in *.${ext}; do
+    if [ $file = "*.${ext}" ] && [ ! -f "*.${ext}" ]; then
+        exit 1;
+    fi
+        rm -f $file
+done
