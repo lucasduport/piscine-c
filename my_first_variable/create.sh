df93343e7e921298cd3e25f1febe888041f17cab
Lucas Duport <lucas.duport@epita.fr>
2023-10-02 20:26:14 +0200

    my_first_variable: tag number 1

---
+++ my_first_variable/create.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+my_local_frais=Javotte
+echo "My frais is ${my_local_frais}"
---
+++ my_first_variable/edit.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+my_local_frais=Javotte
+echo "My frais is ${my_local_frais}"
+my_local_frais=Pulpa
+echo "My frais is now ${my_local_frais}"
