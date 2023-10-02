79beea0e69bae6b91f8d5e8ee83dd8edcab88011
Lucas Duport <lucas.duport@epita.fr>
2023-10-02 21:35:56 +0200

    generate_files: tag number 3

--- generate_files/generate_files.sh
+++ generate_files/generate_files.sh
@@ -5,26 +5,29 @@ nb=1
 ext=txt
 
 while [ $# -gt 1 ]; do
-  case $1 in
+    case $1 in
     
     "-f" | "--filename")
-      fn=$2
-      ;;
+        fn=$2
+        ;;
 
     "-n" | "--number")
-      nb=$2
+        nb=$2
+        if ! [[ "$nb" =~ [0-9]+ ]]; then
+          exit 1
+        fi
       ;;
 
     "-e" | "--extension")
-      ext=$2
-      ;;
+        ext=$2
+        ;;
     *)
-      exit 1
-      ;;
-  esac
-  shift 2
+        exit 1
+        ;;
+    esac
+    shift 2
 done
 
 for i in $(seq 1 $nb); do
-    touch "${fn}-${i}.${ext}"
+    touch -- "${fn}-${i}.${ext}"
 done
