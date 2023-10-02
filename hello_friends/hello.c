c4e82b0c7257b05c783b11168ea37f068ec67810
Lucas Duport <lucas.duport@epita.fr>
2023-10-02 20:51:22 +0200

    hello_friends: tag number 1

---
+++ hello_friends/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        printf("Hello World!\n");
+    }
+    for(int i=1; i < argc; i++)
+    {
+        printf("Hello %s!\n", argv[i]);
+
+    }
+    return 0;
+}
