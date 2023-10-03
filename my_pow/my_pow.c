9591564081c69dccd5b9b5e4601da7cde86baf06
Lucas Duport <lucas.duport@epita.fr>
2023-10-03 15:32:31 +0200

    my_pow: tag number 2

--- my_pow/my_pow.c
+++ my_pow/my_pow.c
@@ -1,8 +1,17 @@
+static int opt_pow(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    if (b % 2 == 0)
+        return opt_pow(a * a, b / 2);
+    return a * opt_pow(a * a, ( b - 1 ) / 2);
+}
+
 int my_pow(int a, int b)
 {
     if (b == 0)
         return 1;
-    if (a % 2)
-        return my_pow(a * a, b / 2);
-    return my_pow(a * a, ( b - 1 ) / 2);
+    if (a == 0)
+        return 0;
+    return opt_pow(a, b);
 }
