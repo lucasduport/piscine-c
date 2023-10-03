dd90d663447beffeccfdfb0c54fd0eb661eb0496
Lucas Duport <lucas.duport@epita.fr>
2023-10-03 20:36:39 +0200

    palindrome: tag number 4

--- palindrome/palindrome.c
+++ palindrome/palindrome.c
@@ -2,17 +2,42 @@
 
 #include <stddef.h>
 
+static int isAlphaNum (char c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return 1;
+    if (c >= 'a' && c <= 'z')
+        return 1;
+    if (c >= '0' && c <= '9')
+        return 1;
+    return 0;
+}
+
 int palindrome(const char *s)
 {
     if (s == NULL)
         return 0;
-    size_t len = 0;
-    for (; s[len] != 0; len++)
-        continue;
-    for (size_t i = 0; i < len / 2; i++)
+    int len = 0;
+    for (int i = 0; s[len] != 0; i++)
+        len++;
+    int i = 0;
+    int j = len - 1;
+    while (i < len && j >= 0)
     {
-        if (s[i] != s[len - 1 - i])
+        if (isAlphaNum(s[i]) != 1)
+        {
+            i++;
+            continue;
+        }
+        if (isAlphaNum(s[j]) != 1)
+        {
+            j--;
+            continue;
+        }
+        if (s[i] != s[j])
             return 0;
+        i++;
+        j--;
     }
     return 1;
 }
