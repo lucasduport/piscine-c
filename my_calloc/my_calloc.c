b086320b02d7c9d4a8749f321991f1e869a32215
Lucas Duport <lucas.duport@epita.fr>
2023-10-03 21:54:22 +0200

    my_calloc: tag number 2

--- my_calloc/my_calloc.c
+++ my_calloc/my_calloc.c
@@ -2,13 +2,14 @@
 
 void *my_calloc(size_t n, size_t size)
 {
-    char *p = malloc(n * size);
+    void *p = malloc(n * size);
     if (p == NULL)
         return NULL;
+    
+    char *ptr = p;
     for (size_t i = 0; i < n; i++)
     {
-        *(p + i) = 0;
+        *(ptr + i) = 0;
     }
-    void *voidP = p;
-    return voidP;
+    return p;
 }
