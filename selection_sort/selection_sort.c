1b2077933eab11bca6598370318940d2b20617be
Lucas Duport <lucas.duport@epita.fr>
2023-10-03 17:40:40 +0200

    selection_sort: tag number 2

--- selection_sort/selection_sort.c
+++ selection_sort/selection_sort.c
@@ -4,14 +4,14 @@ unsigned array_min(const int arr[], unsigned start, unsigned size)
 {
     int mini = arr[start];
     unsigned miniInd = start;
-        for (size_t i = start; i < size; i++)
+    for (size_t i = start; i < size; i++)
+    {
+        if (arr[i] < mini)
         {
-            if (arr[i] < mini)
-            {
-                mini = arr[i];
-                miniInd = i;
-            }
+            mini = arr[i];
+            miniInd = i;
         }
+    }
     return miniInd;
 }
 
