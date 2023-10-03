faf0a2ad4346c3d55f6ac561412a4c3bce755707
Lucas Duport <lucas.duport@epita.fr>
2023-10-03 16:34:52 +0200

    pi_generator: tag number 1

---
+++ pi_generator/pi_generator.c
@@ -0,0 +1,14 @@
+double pi_gen(double i, double p)
+{
+    if (p == i)
+        return 2;
+    return (1 + (i / (i * 2 + 1)) * pi_gen(i + 1, p));
+}
+
+double pi_generator(int precision)
+{
+    if (precision <= 0)
+        return 2;
+    return 2 * pi_gen(1, precision);
+
+}
