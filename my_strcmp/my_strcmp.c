767baa91f0a65b68664fdbb70d89518e96660a0a
Lucas Duport <lucas.duport@epita.fr>
2023-10-03 22:34:40 +0200

    my_strcmp: tag number 1

---
+++ my_strcmp/my_strcmp.c
@@ -0,0 +1,9 @@
+#include "my_strcmp.h"
+
+int my_strcmp(const char *s1, const char *s2)
+{
+    size_t i = 0;
+    for(; s1[i] == s2[i] && s1[i] != 0; i++)
+        i++;
+    return s1[i] == s2[i] && s1[i] == 0;
+}
---
+++ my_strcmp/my_strcmp.h
@@ -0,0 +1,8 @@
+#ifndef MY_STRCMP_H
+#define MY_STRCMP_H
+
+#include <stddef.h>
+
+int my_strcmp(const char *s1, const char *s2);
+
+#endif /* ! MY_STRCMP_H */
