b2fd831a530a9f67604dc4326e11ff826a041a9c
Lucas Duport <lucas.duport@epita.fr>
2023-10-03 22:18:22 +0200

    my_strcpy: tag number 1

---
+++ my_strcpy/my_strcpy.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+
+char *my_strcpy(char *dest, const char *source)
+{
+    size_t i = 0;
+    for (; source[i] != 0; i++)
+    {
+        dest[i] = source[i];
+    }
+    dest[i] = 0;
+    return dest;
+}
