3a5169d17d8377146718fad126bb4afe5ae152cf
Lucas Duport <lucas.duport@epita.fr>
2023-10-03 22:37:41 +0200

    my_strcmp: tag number 2

--- my_strcmp/my_strcmp.c
+++ my_strcmp/my_strcmp.c
@@ -4,6 +4,6 @@ int my_strcmp(const char *s1, const char *s2)
 {
     size_t i = 0;
     for(; s1[i] == s2[i] && s1[i] != 0; i++)
-        i++;
+        continue;
     return s1[i] == s2[i] && s1[i] == 0;
 }
