77fb503caa2b496f864d35ec07137cc996bfba98
Lucas Duport <lucas.duport@epita.fr>
2023-10-03 14:24:54 +0200

    display_square: tag number 1

---
+++ display_square/display_square.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void display_square(int width)
+{
+    if (width <= 0)
+        return;
+    if (width % 2 == 0)
+        width++;
+    int rows = (width + 1) / 2;
+    for(int i = 0; i < width; i++)
+        putchar('*');
+    putchar('\n');
+    for (int i = 0; i < rows - 2; i++)
+    {
+        putchar('*');
+        for (int j = 0; j < width - 2; j++)
+            putchar(' ');
+        putchar('*');
+        putchar('\n');
+    }
+    for(int i = 0; i < width; i++)
+        putchar('*');
+    putchar('\n');
+}
