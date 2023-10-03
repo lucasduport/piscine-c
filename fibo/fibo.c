75ce6550e01fcd73c5bef16deae36e7d431502a1
Lucas Duport <lucas.duport@epita.fr>
2023-10-03 12:58:23 +0200

    fibo: tag number 2

--- fibo/fibo.c
+++ fibo/fibo.c
@@ -1,8 +1,8 @@
-unsigned long fibonacci(unsigned long  n)
+unsigned long fibonacci(unsigned long n)
 {
-  if (n == 0)
-    return 0;
-  if (n == 1)
-    return 1;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
