b24b85868001c7a034106ea76793637397f87ed8
Lucas Duport <lucas.duport@epita.fr>
2023-10-03 22:44:00 +0200

    array_max_min: tag number 2

--- array_max_min/array_max_min.c
+++ array_max_min/array_max_min.c
@@ -4,7 +4,7 @@ void array_max_min(int tab[], size_t len, int *max, int *min)
 {
     *max = *tab;
     *min = *tab;
-    for(size_t i=0; i < len; i++)
+    for(size_t i = 0; i < len; i++)
     {
         if (tab[i] > *max)
             *max = tab[i];
