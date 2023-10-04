69c5baad92bdb6ada079a94703b4a98ca800f9cd
Lucas Duport <lucas.duport@epita.fr>
2023-10-04 21:59:31 +0200

    levenshtein: tag number 1

---
+++ levenshtein/levenshtein.c
@@ -0,0 +1,40 @@
+#include "levenshtein.h"
+
+size_t max(size_t a, size_t b)
+{
+    return (a > b) ? a : b;
+}
+
+size_t min(size_t a, size_t b)
+{
+    return (a < b) ? a : b;
+}
+
+size_t cmp_chr(char c1, char c2)
+{
+    return (c1 == c2) ? 0 : 1;
+}
+
+size_t rec_lev(const char *s1, const char *s2, size_t i, size_t j)
+{
+    if (min(i, j) == 0)
+    {
+        return max(i, j);
+    }
+    return min(rec_lev(s1, s2, i - 1, j) + 1,
+               min(rec_lev(s1, s2, i, j - 1) + 1,
+                   rec_lev(s1, s2, i - 1, j - 1) + cmp_chr(s1[i], s2[j])));
+}
+
+size_t len_s(const char *s)
+{
+    size_t i = 0;
+    for (; s[i] != 0; i++)
+        continue;
+    return i;
+}
+
+size_t levenshtein(const char *s1, const char *s2)
+{
+    return rec_lev(s1, s2, len_s(s1), len_s(s2));
+}
