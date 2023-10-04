3383f92ac5d42c86757251986cf4e17f2b051493
Lucas Duport <lucas.duport@epita.fr>
2023-10-04 13:13:21 +0200

    my_atoi: tag number 4

--- my_atoi/my_atoi.c
+++ my_atoi/my_atoi.c
@@ -6,28 +6,45 @@ int my_pow(int n, int p)
         return 1;
     return n * my_pow(n, p - 1);
 }
+
+int isInvalid(const char *str)
+{
+    int i = 0;
+    int num = 0;
+    int sign = 0;
+    for (; str[i] != 0; i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+            num = 1;
+        else if (str[i] == '+' || str[i] == '-')
+        {
+            if (sign == 1)
+                return 0;
+            sign = 1;
+        }
+        else if (str[i] == ' ' && num + sign == 0)
+            continue;
+        else
+            return 0;
+    }
+    return 1;
+}
+
 int my_atoi(const char *str)
 {
-    int nbDigits = 0;
-    int sign = 1;
-    int nbSign = 0;
     int len = 0;
     for (; str[len] != 0; len++)
         continue;
+    if (isInvalid(str) == 0)
+        return 0;
     int res = 0;
+    int nbDigits = 0;
+    int sign = 1;
     for (int i = len - 1; i >= 0; i--)
     {
-        if (str[i] == ' ')
+        if (str[i] == '-' || str[i] == '+')
         {
-            if (nbDigits != 0 || nbSign != 0)
-                return 0;
-        }
-        else if (str[i] == '-' || str[i] == '+')
-        {
-            if (nbSign > 0)
-                return 0;
             sign = (str[i] == '+') ? 1 : -1;
-            nbSign++;
         }
         else if (str[i] >= '0' && str[i] <= '9')
         {
