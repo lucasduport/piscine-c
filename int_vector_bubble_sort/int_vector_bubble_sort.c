ebbbb7a1a1739f348de674aaef8fdfcfe1c93508
Lucas Duport <lucas.duport@epita.fr>
2023-10-05 22:54:29 +0200

    int_vector_bubble_sort: tag number 3

--- int_vector_bubble_sort/int_vector_bubble_sort.c
+++ int_vector_bubble_sort/int_vector_bubble_sort.c
@@ -35,30 +35,3 @@ struct int_vector int_vector_bubble_sort(struct int_vector vec)
     }
     return vec;
 }
-
-#include <stdio.h>
-int main(void)
-{
-    const size_t size = 35;
-    struct int_vector t = { .size = size,
-                            .data = {
-                                5,    8,    90,   3,    7,     64,   10224,
-                                88,   39,   78,   20,   6,     9,    79,
-                                30,   45,   908,  201,  73,    460,  1330,
-                                37,   32,   13,   709,  310,   1998, 2000,
-                                2020, 2021, 2022, 5600, 10000, 4560, 4800,
-                            } };
-
-    swap(t, 0, 1);
-    printf("Before sorting:");
-    for (size_t i = 0; i < size; i++)
-        printf(" %d", t.data[i]);
-
-    t = int_vector_bubble_sort(t);
-
-    printf("\nAfter sorting:");
-    for (size_t i = 0; i < size; i++)
-        printf(" %d", t.data[i]);
-
-    return 0;
-}
