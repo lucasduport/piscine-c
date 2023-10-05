41076ae561392a52f003e262d64282a17cddbbfc
Lucas Duport <lucas.duport@epita.fr>
2023-10-05 21:42:00 +0200

    int_vector_max: tag number 2

--- int_vector_max/int_vector_max.c
+++ int_vector_max/int_vector_max.c
@@ -1,6 +1,6 @@
 #include "int_vector_max.h"
 
-int int_vector_vice_max(const struct int_vector vec)
+int int_vector_max(const struct int_vector vec)
 {
     int max = vec.data[0];
     for (size_t i = 0; i < vec.size; i++)
