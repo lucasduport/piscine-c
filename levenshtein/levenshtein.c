bda8a1ec9092156d6a288d041bc46dea45ce564f
Lucas Duport <lucas.duport@epita.fr>
2023-10-05 13:24:24 +0200

    levenshtein: tag number 8

--- levenshtein/levenshtein.c
+++ levenshtein/levenshtein.c
@@ -2,11 +2,6 @@
 
 #include <stdlib.h>
 
-size_t max(size_t a, size_t b)
-{
-    return (a > b) ? a : b;
-}
-
 size_t min(size_t a, size_t b)
 {
     return (a < b) ? a : b;
@@ -15,19 +10,6 @@ size_t min(size_t a, size_t b)
 size_t cmp_chr(char c1, char c2)
 {
     return (c1 == c2) ? 0 : 1;
-}
-
-size_t rec_lev(const char *s1, const char *s2, size_t i, size_t j)
-{
-    if (min(i, j) == 0)
-    {
-        return max(i, j);
-    }
-    if (s1[i] == s2[j])
-        return rec_lev(s1, s2, i - 1, j - 1);
-    return 1
-        + min(rec_lev(s1, s2, i - 1, j),
-              min(rec_lev(s1, s2, i, j - 1), rec_lev(s1, s2, i - 1, j - 1)));
 }
 
 size_t len_s(const char *s)
@@ -41,7 +23,8 @@ size_t len_s(const char *s)
 size_t levenshtein(const char *s1, const char *s2)
 {
     size_t lenS1 = len_s(s1);
-    size_t *col1 = malloc(sizeof(int) * lenS1 + 1);
+    size_t *col1 = malloc(sizeof(size_t) * (lenS1 + 1));
+    col1[0] = 0;
     size_t lenS2 = len_s(s2);
     for (size_t j = 1; j <= lenS1; j++)
     {
@@ -51,7 +34,7 @@ size_t levenshtein(const char *s1, const char *s2)
     {
         col1[0] = i;
         size_t der_d = i - 1;
-        for (size_t j = 0; j < lenS1; j++)
+        for (size_t j = 1; j <= lenS1; j++)
         {
             size_t anc_d = col1[j];
             col1[j] = min(min(col1[j] + 1, col1[j - 1] + 1),
