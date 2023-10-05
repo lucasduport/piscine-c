a96ac357744d243edbe9f6c2a3efa2cdaec55e80
Lucas Duport <lucas.duport@epita.fr>
2023-10-05 13:13:27 +0200

    levenshtein: tag number 7

--- levenshtein/levenshtein.c
+++ levenshtein/levenshtein.c
@@ -1,5 +1,7 @@
 #include "levenshtein.h"
 
+#include <stdlib.h>
+
 size_t max(size_t a, size_t b)
 {
     return (a > b) ? a : b;
@@ -38,11 +40,26 @@ size_t len_s(const char *s)
 
 size_t levenshtein(const char *s1, const char *s2)
 {
-    static int tst = 0;
-    tst++;
-    if (tst == 11)
+    size_t lenS1 = len_s(s1);
+    size_t *col1 = malloc(sizeof(int) * lenS1 + 1);
+    size_t lenS2 = len_s(s2);
+    for (size_t j = 1; j <= lenS1; j++)
     {
-        return 8;
+        col1[j] = j;
     }
-    return rec_lev(s1, s2, len_s(s1), len_s(s2));
+    for (size_t i = 1; i <= lenS2; i++)
+    {
+        col1[0] = i;
+        size_t der_d = i - 1;
+        for (size_t j = 0; j < lenS1; j++)
+        {
+            size_t anc_d = col1[j];
+            col1[j] = min(min(col1[j] + 1, col1[j - 1] + 1),
+                          der_d + cmp_chr(s1[j - 1], s2[i - 1]));
+            der_d = anc_d;
+        }
+    }
+    size_t res = col1[lenS1];
+    free(col1);
+    return res;
 }
