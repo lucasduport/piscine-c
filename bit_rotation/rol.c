cba2c79a2576e7978b204c78678500c7604ed812
Lucas Duport <lucas.duport@epita.fr>
2023-10-09 17:01:31 +0200

    bit_rotation: tag number 2

--- bit_rotation/rol.c
+++ bit_rotation/rol.c
@@ -1,6 +1,7 @@
 unsigned char rol(unsigned char value, unsigned char roll)
 {
-    unsigned char roled = value << roll;
-    unsigned char leftPart = value >> ((sizeof(unsigned char) * 8) - roll);
+    roll = roll % 8;
+    unsigned int roled = value << roll;
+    unsigned int leftPart = value >> ((sizeof(unsigned char) * 8) - roll);
     return roled | leftPart;
 }
