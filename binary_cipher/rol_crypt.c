2b13ba31663ddb51f5d8f2e1afbee817da46af6f
Lucas Duport <lucas.duport@epita.fr>
2023-10-10 12:32:31 +0200

    binary_cipher: tag number 1

---
+++ binary_cipher/rol_crypt.c
@@ -0,0 +1,16 @@
+#include "binary_cipher.h"
+
+static char char_crypt(char c, char key)
+{
+    return c + key;
+}
+
+void my_rol_crypt(void *data, size_t data_len, const void *key, size_t key_len)
+{
+    char *message = data;
+    const char *k = key;
+    for (size_t i = 0; i < data_len; i++)
+    {
+        message[i] = char_crypt(message[i], k[i % key_len]);
+    }
+}
