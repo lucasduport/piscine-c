7ae6b4771335743aabffc7dfd9708f2c3f70888b
Lucas Duport <lucas.duport@epita.fr>
2023-10-10 18:41:49 +0200

    my_memcpy: tag number 2

--- my_memcpy/my_memcpy.c
+++ my_memcpy/my_memcpy.c
@@ -4,9 +4,14 @@ void *my_memcpy(void *dest, const void *source, size_t num)
 {
     char *cDst = dest;
     const char *cSrc = source;
-    for (size_t i = 0; i < num && dest + i < source; i++)
+    for (size_t i = 0; i < num && (cSrc + i) < cDst; i++)
     {
         cDst[i] = cSrc[i];
     }
     return dest;
+}
+
+int main(void)
+{
+    return 0;
 }
