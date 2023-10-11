c4be04d551beb920770f966c0cae0fe05035e42d
Lucas Duport <lucas.duport@epita.fr>
2023-10-11 12:25:11 +0200

    spoilers: tag number 4

--- spoilers/spoilers.c
+++ spoilers/spoilers.c
@@ -3,14 +3,36 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int process(int f_read)
+int handle_args(int argc, char *argv[])
 {
-    char res[] = "  ";
+    if (argc != 2)
+    {
+        write(STDERR_FILENO, "Invalid number of arguments\n", 28);
+        ;
+        return -1;
+    }
+    int f_read = open(argv[1], O_RDONLY);
+    if (f_read == -1)
+    {
+        write(STDERR_FILENO, "Could not open file\n", 20);
+        return -1;
+    }
+    close(f_read);
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    if (handle_args(argc, argv) == -1)
+        return 1;
+    int f_read = open(argv[1], O_RDONLY);
+    char res[] = "  \n";
     int j = 1;
     char buff[5] = "";
     ssize_t bytesRead = 0;
+    int finish = 0;
     int r = 1;
-    while (j < 0 && r != 0)
+    while (!finish && r != 0)
     {
         off_t s = lseek(f_read, -5 - bytesRead, SEEK_END);
         if (s == -1)
@@ -27,32 +49,19 @@ int process(int f_read)
                 res[j] = buff[i];
                 j--;
                 if (j < 0)
+                {
+                    finish = 1;
                     break;
+                }
             }
         }
     }
-    if (j < 0)
+    close(f_read);
+    if (finish == 0)
     {
         write(STDERR_FILENO, "File is not long enough\n", 24);
         return 1;
     }
-    write(STDOUT_FILENO, res, 2);
-    write(STDOUT_FILENO, "\n", 1);
+    write(STDOUT_FILENO, res, 3);
     return 0;
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        write(STDERR_FILENO, "Invalid number of arguments\n", 28);
-        return 1;
-    }
-    int f_read = open(argv[1], O_RDONLY);
-    if (f_read == -1)
-    {
-        write(STDERR_FILENO, "Could not open file\n", 20);
-        return 1;
-    }
-    return process(f_read);
 }
