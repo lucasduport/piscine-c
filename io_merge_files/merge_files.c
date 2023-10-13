4ecb8257dd0c8d08dd3ede34b0638091f1f5d1d7
Lucas Duport <lucas.duport@epita.fr>
2023-10-13 10:09:56 +0200

    io_merge_files: tag number 2

--- io_merge_files/merge_files.c
+++ io_merge_files/merge_files.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-
 #define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int merge_files(const char *file_1, const char *file_2)
 {
@@ -10,14 +11,17 @@ int merge_files(const char *file_1, const char *file_2)
     FILE *f_write = fopen(file_1, "a");
     if (f_write == NULL)
         return -1;
-    char buff[20] = "";
     size_t buffSize = 20;
-    size_t r;
-    while ((r = fread(buff, sizeof(char), buffSize, f_read)) != 0)
+    char *buff = malloc(sizeof(char) * buffSize);
+    ssize_t r;
+    while ((r = getline(&buff, &buffSize, f_read)) != -1)
     {
-        size_t wr = fwrite(buff, sizeof(char), r, f_write);
-        if (wr == 0)
+        if (buff == NULL)
+            return -1;
+        int wr = fputs(buff, f_write);
+        if (wr < 0)
             return -1;
     }
+    free(buff);
     return 0;
 }
