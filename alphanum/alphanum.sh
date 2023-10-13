f6d05f0dd06895f62759510849359db13820198d
Lucas Duport <lucas.duport@epita.fr>
2023-10-13 08:02:55 +0200

    alphanum: tag number 2

--- alphanum/alphanum.sh
+++ alphanum/alphanum.sh
@@ -2,17 +2,17 @@
 
 while IFS='' read -r line; do
     if [  $(($(echo -n "$line" | grep -c -Ex "^[0-9]$" ))) -eq 1 ]; then
-        echo "it is digit"
+        echo "it is a digit"
     elif [  $(($(echo -n "$line" | grep -c -Ex "^[0-9]+$" ))) -eq 1 ]; then
         echo "it is a number"
     elif [  $(($(echo -n "$line" | grep -c -Ex "^[a-zA-Z]+$" ))) -eq 1 ]; then
         echo "it is a word"
     elif [  $(($(echo -n "$line" | grep -c -Ex "^[a-zA-Z0-9]+$" ))) -eq 1 ]; then
         echo "it is an alphanum"
-    elif [  $(($(echo -n "$line" | grep -c -Ex "^[ \t]*$" ))) -eq 1 ]; then
+    elif [  $(($(echo -n "$line" | grep -c -Px '( |\t)*' ))) -ge 1 ]\
+        || [ "$line" = '' ]; then
         echo "it is empty"
     else
-        printf %s $line
         echo "it is too complicated"
     fi
 done
